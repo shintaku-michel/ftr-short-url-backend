f1e8a5e043c695cf6517540e538aa625aaf1e981
Michel Shintaku Martins <michel.martins@camara.leg.br>
2025-09-07 20:52:59 -0300

    setup projeto

---
+++ src/infra/http/server.ts
@@ -0,0 +1,12 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.register(fastifyCors, {
+    origin: '*',
+});
+
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+    console.log('HTTP server running on http://localhost:3333');
+});
